cbe2ca3c3f56ba1c646a880d4868d2cb12d2bff9
Lucase1840 <l.ucase1840@gmail.com>
2024-12-17 20:58:51 -0300

    chore: project initialization
    - Initial project setup.
    
    - Linters and formatters were added.
    
    - Initial dependencies were added.
    
    - Css styles were reset.

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <h1>Vite + React</h1>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+})
